f578f15e4da1d7c9770f27a49b70c12e21e78121
iSweazZ <41954265+iSweazZ@users.noreply.github.com>
2018-07-31 14:05:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ const Discord = require('discord.js');
 
 const Bot = new Discord.Client();
 
+var dernier_message = new Array()
+
 Bot.on('ready', () => {
 
-Bot.user.setPresence({ game: { name: ':D Vive BAWA Games', type: 0 } });
-console.log("Bot en ligne");
+Bot.user.setPresence({ game: { name: 'Vive BAWA musique :D', type: 0 } });
+console.log("Bot musique en ligne");
 
 });
 
-Bot.login('NDczNDgxNzI1OTk4MDA2Mjgy.DkDJKg.LriabaAvdEnLqK9Nwny1wA58ZCE')
+Bot.login('NDczNjAxMzMwMDkxNjU1MTg4.DkEUCA.V4IbQkeAK5jTeOXKMbLgwYdry4c')
 
 
 Bot.on('message', message => {
 
-    var commande = "*";
-    var existe = "0"
+    console.log(message.channel.name.toLowerCase() + " , " + message.channel.name.toLowerCase() == "général")
+    if (message.channel.name.toLowerCase() == "général"){
+    
+    var commande = "!";
 
     if (message.content.toLowerCase().startsWith(commande + "hello")){//Bonjour
         
-        message.channel.send("Salut " + message.member.displayName + " Permission(s): " + message.member.permissions.hasPermission("ADMINISTRATOR"));
+        message.channel.send("Salut " + message.member.displayName);
         
     }
     
@@ -42,11 +46,11 @@ Bot.on('message', message => {
 
     if (message.content.toLowerCase().startsWith(commande + "help")){
         help_embed.setColor('#1801F7')
-        help_embed.addField("Commande du bot BAWA Assistance", "*help : Afficher les commandes \n *hello : pour me dire bonjour")
-        help_embed.addField("Intéragir avec le bot", "*game: modifier le nom du jeu (bot)")
+        help_embed.addField("Commande du bot BAWA Musique", "!help : Afficher les commandes \n !hello : pour me dire bonjour \n !musique + url : joue la musique présente à l'URL indiqué \n !sa vas : demander si le bot va bien \n !oui : donner une réponse positive au bot \n !non : donner une réponse négative au bot")        
+        help_embed.addField("Intéragir avec le bot", "!game: modifier le nom du jeu (bot)")
         if (message.member.hasPermission('ADMINISTRATOR')){
         help_embed.setColor('#FD0103')
-        help_embed.addField("Commande BAWA Assistance (Admin)", "*arret pour arrêter le bot")
+        help_embed.addField("Commande BAWA Assistance (Admin)", "!arret pour arrêter le bot")
         }
         message.channel.sendEmbed(help_embed)
     }
@@ -57,5 +61,40 @@ Bot.on('message', message => {
         jeu.shift()
         jeu = jeu.join(' ')
         Bot.user.setPresence({ game: { name: jeu, type: 0 } });
+    }
+    
+    
+
+    if (message.content.toLowerCase().startsWith(commande + "sa vas")){
+        var index = 0
+        var phrase = new Array("je vais bien et toi?", "Je vais bien, merci de te soucier de moi :)", "Je vais parfaitement bien, si tu veux de la musique dis le moi ;)")
+        index = getRandomInt(0, 3);
+        console.log(index)
+        dernier_message[0] = phrase[index]
+        dernier_message[1] = message.member.displayName
+        console.log(dernier_message[0])
+        message.reply(dernier_message[0]);
+        if (index == 0){
+            console.log(dernier_message[0] + " " + dernier_message[1])
+        }
+    }
+
+    if (dernier_message[0] == "je vais bien et toi?" && dernier_message[1] == message.member.displayName){
+        if (message.content.toLowerCase().startsWith(commande + "oui")){
+        console.log("répondu oui")
+        message.reply("Je suis heureux de savoir que toi aussi, tu aille bien :)");
+        dernier_message[0] = ""
+        }else if ((message.content.toLowerCase().startsWith(commande + "non"))){
+            message.reply("Je suis désolé de savoir que tu vas mal :( mais donne moi un URL de musique pour tu remonter le moral :)")
+            dernier_message[0] = ""
+        }
+    }
+
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
       }
+    }
 });
